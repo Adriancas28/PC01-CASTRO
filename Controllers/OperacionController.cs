137230521321b0181bef5f68357f29e8617ee154
Adriancas28 <castroadrian1228@gmail.com>
2024-08-24 14:30:18 -0500

    cambios

--- Controllers/OperacionController.cs
+++ Controllers/OperacionController.cs
@@ -11,16 +11,55 @@ namespace PC01.Controllers
     [Route("[controller]")]
     public class OperacionController : Controller
     {
-        private readonly ILogger<OperacionController> _logger;
+         private readonly ILogger<OperacionController> _logger;
 
         public OperacionController(ILogger<OperacionController> logger)
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(Operacion operacion)
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Registrar(Operacion operacion)
+        {
+            if (operacion.Instrumentos == null || !operacion.Instrumentos.Any())
+            {
+                ModelState.AddModelError("", "Debe seleccionar al menos un instrumento");
+                return View("Index", operacion);
+            }
+
+            // Definir precios de los instrumentos
+            var precios = new Dictionary<string, double>
+            {
+                { "S&P 500", 500 },
+                { "Dow Jones", 300 },
+                { "Bonos US", 120 }
+            };
+
+            // Calcular el monto total de los instrumentos seleccionados
+            double montoTotalInstrumentos = operacion.Instrumentos.Sum(instrumento => precios.ContainsKey(instrumento) ? precios[instrumento] : 0);
+
+          
+
+            // Calcular Comisión basada en el monto ingresado
+            double montoAbonado = operacion.Monto.HasValue ? operacion.Monto.Value : 0;
+            double comision = montoAbonado <= 300 ? 3 : 1;
+            double igv = montoAbonado * 0.18;
+            // Calcular Total a Pagar
+            double total = montoAbonado + igv + comision;
+
+            // Pasar los valores calculados a la vista
+            ViewData["Instrumentos"] = string.Join(", ", operacion.Instrumentos);
+            ViewData["FecOpe"] = operacion.FecOpe.ToString("dd/MM/yyyy");
+            ViewData["IGV"] = igv.ToString("F2");
+            ViewData["Comision"] = comision.ToString("F2");
+            ViewData["Total"] = total.ToString("F2");
+            ViewData["Monto"] = montoAbonado.ToString("F2");
+
+            return View("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -28,5 +67,15 @@ namespace PC01.Controllers
         {
             return View("Error!");
         }
+    }
+
+    public class Operacion{
+        public string? Nombre { get; set;}
+        public string? Apellido { get;set;}
+        public string? Correo { get; set;}
+        public DateOnly FecOpe { get; set;}
+        public List<string>? Instrumentos { get; set;}
+        public double? Monto { get; set;}
+    }
     }
 }
